1b93895f5b7d2473e7fe8a44d838887bd8974d2d
Joseph Sheedy <joseph.sheedy@gmail.com>
2024-04-25 22:08:23 -0700

    WIP

--- arduino/u_cube/nanofont.h
+++ arduino/u_cube/nanofont.h
@@ -29,7 +29,7 @@ void draw_nanofont_char(CRGB *leds, short ul_x, short ul_y, uint8_t idx, CRGB co
   }
 }
 
-void nanofont(CRGB *leds) {
+void nanofont_test(CRGB *leds) {
   CRGB color = ColorFromPalette(RainbowColors_p, millis() / 25);
   uint8_t idx = 32;
   for (uint8_t y = HEIGHT - 1; y > 3; y -= 4) {
@@ -38,6 +38,24 @@ void nanofont(CRGB *leds) {
       idx++;
     }
   }
+}
+
+void nanofont(CRGB *leds) {
+  CRGB color = ColorFromPalette(RainbowColors_p, millis() / 25);
+
+  draw_nanofont_char(leds, 0, 22, 35, color); // C
+  draw_nanofont_char(leds, 4, 22, 53, color); // U
+  draw_nanofont_char(leds, 8, 22, 52, color); // T
+  draw_nanofont_char(leds, 12, 22, 41, color); // I
+  draw_nanofont_char(leds, 16, 22, 37, color); // E
+
+  draw_nanofont_char(leds, 0, 18, 38, color); // F
+  draw_nanofont_char(leds, 4, 18, 41, color); // I
+  draw_nanofont_char(leds, 8, 18, 33, color); // A
+  draw_nanofont_char(leds, 12, 18, 46, color); // N
+  draw_nanofont_char(leds, 16, 18, 35, color); // C
+  draw_nanofont_char(leds, 20, 18, 37, color); // E
+
 }
 
 #endif
